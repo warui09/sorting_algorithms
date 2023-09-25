9676482bb0dabe07ceb2b6d6f1c6025108843bea
Arnold Henry <devharnold@gmail.com>
2023-09-25 18:57:41 +0300

    header

--- sort.h
+++ sort.h
@@ -17,10 +17,26 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/**
+ * swap_ints - swap two numbers in an array.
+ * @a: first num
+ * @b: second num
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 
 /* prototypes */
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap_ints(int *a, int *b);
+void bubble_sort(int *array, size_t size);
 
 #endif /* SORT_H */
