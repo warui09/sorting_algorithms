59b1af4a95b154f07c1930ae1b029d295ca79509
Arnold Henry <devharnold@gmail.com>
2023-09-27 22:48:23 +0300

    make-change

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,6 +1,26 @@
 #include "sort.h"
 
 /**
+ * get_max - find max value in array
+ * @array: array
+ * @size: array size
+ *
+ * Return: max integer in array
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
  * counting_sort - sort array with counting method
  * @array: array to sort
  * @size: size of array
--- sort.h
+++ sort.h
@@ -17,6 +17,26 @@ typedef enum bool
 	true
 } bool;
 
+/**
+ * get_max - find max value in array
+ * @array: array
+ * @size: array size
+ *
+ * Return: max integer in array
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
 /* doubly linked list struct */
 
 /**
